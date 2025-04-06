833b4a96d06ff4a30f184da644230b83b653e5bb
HANOU Auster Noukpo <arishn02@gmail.com>
2025-04-06 09:57:46 +0200

    update files

--- memory.js
+++ memory.js
@@ -5,8 +5,8 @@ var champPos = document.getElementById("pos");
 var champData = document.getElementById("data");
 var champCtrl = document.getElementById("ctrl");
 var boutonGo = document.getElementById("go");
-if (!ctx || !champPos || !champData || !champCtrl || !boutonGo || parseInt(champPos.value) <= 0 || parseInt(champPos.value) > 256 || parseInt(champData.value) < 0 || parseInt(champData.value) > 255 || parseInt(champCtrl.value) < 0 || parseInt(champCtrl.value) > 1) {
-    alert("Certains éléments graphiques n'ont pas été trouvés sur la page ou Erreur sur les entrées utilisateur!");
+if (!ctx || !champPos || !champData || !champCtrl || !boutonGo) {
+    alert("Certains éléments graphiques n'ont pas été trouvés sur la page !");
 }
 //fonction pour dessiner une mémoire ainsi les buffers (adresse, donnée et controle)
 //chaque case de la mémoire est d'abord initialiser à 0x00
@@ -50,36 +50,38 @@ function drawMemory() {
 drawMemory();
 function start() {
     //vérification d'erreur sur les intervalles auxquelles doivent appartenir les données saisies par l'utilisateur
-    if (parseInt(champPos.value) <= 0 || parseInt(champPos.value) > 256 || parseInt(champData.value) < 0 || parseInt(champData.value) > 255 || parseInt(champCtrl.value) < 0 || parseInt(champCtrl.value) > 1) {
-        throw new Error("Erreur sur les entrées utilisateur. Vérifiez les valeurs saisies !");
+    if (isNaN(parseInt(champPos.value)) || isNaN(parseInt(champData.value)) || isNaN(parseInt(champCtrl.value)) || parseInt(champPos.value) <= 0 || parseInt(champPos.value) > 256 || parseInt(champData.value) < 0 || parseInt(champData.value) > 255 || parseInt(champCtrl.value) < 0 || parseInt(champCtrl.value) > 1) {
+        alert("Erreur sur les entrées utilisateur. Vérifiez les valeurs saisies !");
     }
-    ctx.clearRect(115, 567, 50, 25);
-    ctx.clearRect(245, 567, 50, 25);
-    ctx.clearRect(375, 567, 50, 25);
-    //Remplissage du buffer d'addresse
-    //Les adresses commencent à 0 dans la mémoire 
-    //Donc pour avoir la bonne adressse, il faudra soustraire 1 de la postion renseignée
-    ctx.fillText("0x" + (parseInt(champPos.value) - 1).toString(16).toUpperCase(), 126, 582);
-    //Remplissage du buffer de données
-    ctx.fillText("0x" + parseInt(champData.value).toString(16).toUpperCase(), 256, 582);
-    //Remplissage du buffer de contrôle
-    ctx.fillText("0x" + parseInt(champCtrl.value).toString(16).toUpperCase(), 386, 582);
-    //si la valeur de l'instruction de contrôle est 1, reset de la mémoire
-    if (parseInt(champCtrl.value) == 1) {
-        //nettoyage de tout le canva pour le redessiner
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawMemory();
-    }
-    //sinon si la valeur de l'instruction de contrôle est 0, on écrit la donnée à la bonne position dans la mémoire
     else {
-        var step = 1; //compteur de pas pour atteindre la position où la donnée doit être insérer
-        for (var i = 1; i <= 32; ++i) {
-            for (var j = 1; j <= 8; ++j) {
-                if (step == parseInt(champPos.value)) {
-                    ctx.clearRect((60 + (j - 1) * (440 / 8)), (20 + (i - 1) * (460 / 31)) - 9, 40, 10);
-                    ctx.strokeText("0x" + parseInt(champData.value).toString(16).toUpperCase(), 60 + (j - 1) * (440 / 8), 20 + (i - 1) * (460 / 31));
+        ctx.clearRect(115, 567, 50, 25);
+        ctx.clearRect(245, 567, 50, 25);
+        ctx.clearRect(375, 567, 50, 25);
+        //Remplissage du buffer d'addresse
+        //Les adresses commencent à 0 dans la mémoire 
+        //Donc pour avoir la bonne adressse, il faudra soustraire 1 de la postion renseignée
+        ctx.fillText("0x" + (parseInt(champPos.value) - 1).toString(16).toUpperCase(), 126, 582);
+        //Remplissage du buffer de données
+        ctx.fillText("0x" + parseInt(champData.value).toString(16).toUpperCase(), 256, 582);
+        //Remplissage du buffer de contrôle
+        ctx.fillText("0x" + parseInt(champCtrl.value).toString(16).toUpperCase(), 386, 582);
+        //si la valeur de l'instruction de contrôle est 1, reset de la mémoire
+        if (parseInt(champCtrl.value) == 1) {
+            //nettoyage de tout le canva pour le redessiner
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawMemory();
+        }
+        //sinon si la valeur de l'instruction de contrôle est 0, on écrit la donnée à la bonne position dans la mémoire
+        else {
+            var step = 1; //compteur de pas pour atteindre la position où la donnée doit être insérer
+            for (var i = 1; i <= 32; ++i) {
+                for (var j = 1; j <= 8; ++j) {
+                    if (step == parseInt(champPos.value)) {
+                        ctx.clearRect((60 + (j - 1) * (440 / 8)), (20 + (i - 1) * (460 / 31)) - 9, 40, 10);
+                        ctx.strokeText("0x" + parseInt(champData.value).toString(16).toUpperCase(), 60 + (j - 1) * (440 / 8), 20 + (i - 1) * (460 / 31));
+                    }
+                    step++;
                 }
-                step++;
             }
         }
     }
